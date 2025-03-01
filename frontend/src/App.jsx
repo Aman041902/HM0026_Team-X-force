4203cd77be09e2ce0a282d8d615f7753f10d8768
Aman Jain <141843384+Aman041902@users.noreply.github.com>
2025-03-02 02:05:45 +0530

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,27 +19,31 @@ import AdminDashboard from "./components/AdminDashboard";
 function App() {
   const role = useSelector((state) => state.auth.role);
   const token = useSelector((state) => state.auth.token);
-  console.log(token, role)
+  console.log(token, role);
   return (
     <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path='/about-us' element={<About/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            { token && role === "student" ? (
-                <Route path="/dashboard/student" element={<StudentDashboard />} />
-            ) : role === "instructor" && token !== null ? (
-                <Route path="/dashboard/instructor" element={<TeacherDashboard />} />
-            ) : role === "admin" && token !== null ? (
-                <Route path="/dashboard/admin" element={<AdminDashboard />} />
-            ) : null}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          
+          {token && role === "student" ? (
+            <Route path="/dashboard/student" element={<StudentDashboard />} />
+          ) : role === "instructor" && token !== null ? (
+            <Route
+              path="/dashboard/instructor"
+              element={<TeacherDashboard />}
+            />
+          ) : role === "admin" && token !== null ? (
+            <Route path="/dashboard/admin" element={<AdminDashboard />} />
+          ) : null}
 
-            {/* <Route path="*" element={<Error />}/> */}
-          </Routes>
-        </BrowserRouter>
+          {/* <Route path="*" element={<Error />}/> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
