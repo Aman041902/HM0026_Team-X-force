fdfcc3f364585107e8101ecad0aca58c321a3958
OnkarJondhale <onkar.jondhale@gmail.com>
2025-03-02 01:12:53 +0530

    fixed router issue and added error component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,17 +9,28 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootreducer from "./Redux/reducer";
 import { Provider, useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Contact from "./components/Contact";
 import { About } from "./components/About";
+import Error from "./components/Error"
 import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
-  const role = useSelector((state) => state.auth.role);
   const token = useSelector((state) => state.auth.token);
-  console.log(token, role)
+
+
+  let role = null;
+
+  if(token)
+  {
+    const decodedToken = jwtDecode(token);
+    role = decodedToken.role;
+  }
+  
+  console.log(token,role);
   return (
     <div>
         <BrowserRouter>
@@ -27,17 +38,25 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path='/about-us' element={<About/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            { token && role === "student" ? (
-                <Route path="/dashboard/student" element={<StudentDashboard />} />
-            ) : role === "instructor" && token !== null ? (
-                <Route path="/dashboard/instructor" element={<TeacherDashboard />} />
-            ) : role === "admin" && token !== null ? (
-                <Route path="/dashboard/admin" element={<AdminDashboard />} />
-            ) : null}
+            <Route path="/about-us" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
 
-            {/* <Route path="*" element={<Error />}/> */}
+            {
+              token && role==='student' && 
+              <Route path="/dashboard/student" element={<StudentDashboard />} />
+            }
+
+            {
+              token && role==='instructor' && 
+              <Route path="/dashboard/instructor" element={<TeacherDashboard />} />
+            }
+
+            {
+              token && role==='admin' && 
+              <Route path="/dashboard/admin" element={<AdminDashboard />} />
+            }
+
+            <Route path="*" element={<Error />} />
           </Routes>
         </BrowserRouter>
     </div>
