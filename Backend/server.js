a2ccc8119050a54c6a8aee63dc60f310aad3fbf6
OnkarJondhale <onkar.jondhale@gmail.com>
2025-03-02 02:17:45 +0530

    fixed routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const connectDb = require("./config/database");
+const { connectMail} = require("./config/mailer");
 const router = require("../Backend/routes/authroute");
+const contactrouter = require("../Backend/routes/contactroute");
 const cors = require("cors");
 const app = express();
+
 app.use(cors());
 
 connectDb();
-
+connectMail();
+ 
 app.use(express.json());
 app.use("/user", router);
+app.use('',contactrouter);
 
 app.listen(3000, () => {
   console.log("server statrted successfully");
